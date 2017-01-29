2bf0be490d578ec7cf34785b7d7cd98b833501a6
Yogesh Kumar <ykumar@nyu.edu>
2017-01-29 18:22:40 -0500

    more exercises

--- src/koans/07_functions.clj
+++ src/koans/07_functions.clj
@@ -7,34 +7,35 @@
 (defn square [n] (* n n))
 
 (meditations
-  "Calling a function is like giving it a hug with parentheses"
-  (= __ (square 9))
+ "Calling a function is like giving it a hug with parentheses"
+ (= 81 (square 9))
 
-  "Functions are usually defined before they are used"
-  (= __ (multiply-by-ten 2))
+ "Functions are usually defined before they are used"
+ (= 20 (multiply-by-ten 2))
 
-  "But they can also be defined inline"
-  (= __ ((fn [n] (* 5 n)) 2))
+ "But they can also be defined inline"
+ (= 10 ((fn [n] (* 5 n)) 2))
 
-  "Or using an even shorter syntax"
-  (= __ (#(* 15 %) 4))
+ "Or using an even shorter syntax"
+ (= 60 (#(* 15 %) 4))
 
-  "Even anonymous functions may take multiple arguments"
-  (= __ (#(+ %1 %2 %3) 4 5 6))
+ "Even anonymous functions may take multiple arguments"
+ (= 15 (#(+ %1 %2 %3) 4 5 6))
 
-  "Arguments can also be skipped"
-  (= __ (#(* 15 %2) 1 2))
+ "Arguments can also be skipped"
+ (= 30 (#(* 15 %2) 1 2))
 
-  "One function can beget another"
-  (= 9 (((fn [] ___)) 4 5))
+ "One function can beget another"
+ (= 9 (((fn [] +)) 4 5))
 
-  "Functions can also take other functions as input"
-  (= 20 ((fn [f] (f 4 5))
-           ___))
+ "Functions can also take other functions as input"
+ (= 20 ((fn [f] (f 4 5))
+        *))
 
-  "Higher-order functions take function arguments"
-  (= 25 (___
-          (fn [n] (* n n))))
+ "Higher-order functions take function arguments")
+(= 25 ((fn [f] (f 5))
+       (fn [n] (* n n))))
 
-  "But they are often better written using the names of functions"
-  (= 25 (___ square)))
+"But they are often better written using the names of functions"
+(= 25 (___ square))
+  
